52255a360c1b5a95f9cdd770e3df24c5fbe6a8dc
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-14 12:55:36 +0700

    Add fonts

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import '@fontsource/poppins/600.css'
+import '@fontsource/poppins/700.css'
+import '@fontsource-variable/inter'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
