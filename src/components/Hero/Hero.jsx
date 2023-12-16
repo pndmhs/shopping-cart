f99d44961dc3496febb1335b19d69f8f27ad488e
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-17 06:16:13 +0700

    Add hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import styles from './Hero.module.css'
+import HeroImage from '../../assets/hero-image.svg?react'
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <div className={styles['hero-text']}>
+        <h1>Everything you need, made easy.</h1>
+        <p>Simple Shop offers a wide variety of goods, from everyday essentials to exciting electronics, all at competitive prices</p>
+        <button>SHOP NOW</button>
+      </div>
+      <div className={styles['hero-image']}>
+        <HeroImage />
+      </div>
+    </div>
+  )
+}
+
+export default Hero
