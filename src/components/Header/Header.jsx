a51369c000691ed7857d7c49a11b915502326e5d
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-16 11:25:50 +0700

    Add search bar and mobile layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,21 @@
 import styles from './Header.module.css'
 import { MdOutlineShoppingBag } from "react-icons/md"
+import { HiOutlineSearch, HiOutlineMenu } from "react-icons/hi";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <h1 className={styles.logo}>SIMPLE <span className={styles[`text-stroke`]}>SHOP</span></h1>
+      <div className={styles['sm-hidden']}>
+        <HiOutlineSearch className={styles['search-icon']}/>
+        <input type='text' name='search' placeholder='Search'/>
+      </div>
       <nav className={styles.nav}>
         <ul>
-          <li>Home</li>
-          <li>Shop</li>
-          <li><MdOutlineShoppingBag size={24}/></li>
+          <li className={styles['sm-hidden']}>Home</li>
+          <li className={styles['sm-hidden']}>Shop</li>
+          <li><MdOutlineShoppingBag size={24} /></li>
+          <li className={`${styles['sm-show']} ${styles['lg-hidden']}`}><HiOutlineMenu size={24} /></li>
         </ul>
       </nav>
     </header>
