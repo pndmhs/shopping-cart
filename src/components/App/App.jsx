0c01d2266856667010f4ed36dff3aec5e82b8463
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-17 09:07:40 +0700

    Add useFetch hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from '../Header/Header'
 import Hero from '../Hero/Hero'
 import styles from './App.module.css'
 
+const useFetch = (url) => {
+  const [data, setData] = useState(null)
+  const [err, setErr] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => setData(data))
+    .catch(err => setErr(err))
+    .finally(() => setLoading(false))
+  }, [])
+
+  return { data, err, isLoading }
+}
+
 const App = () => {
-  
+
   return (
     <div className={styles.container}>
       <Header />
