dc7a49011d6977ed0c3957e7bc79561f4e10bd9e
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-18 22:36:34 +0700

    Add function to fetch and display product on one category

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -9,58 +9,85 @@ const useFetch = (url) => {
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
+    setLoading(true)
+
     fetch(url)
     .then(response => response.json())
     .then(data => setData(data))
     .catch(err => setErr(err))
     .finally(() => setLoading(false))
-  }, [])
+  }, [url])
 
   return { data, err, isLoading }
 }
 
+const ProductList = ({ data }) => {
+  return (
+    <div className={styles['product-container']}>
+      <span>{data.length} items</span>
+      <div className={styles['product-list']}>
+        {data.map(product => <Product data={product} key={product.id}/>)}
+      </div>
+    </div>
+  )
+}
+
 const Shop = () => {
-  const { data, err, isLoading } = useFetch('https://fakestoreapi.com/products')
+  const [url, setUrl] = useState('https://fakestoreapi.com/products')
+
+  const { data, err, isLoading } = useFetch(url)
 
   const [activeCategory, setActiveCategory] = useState(null)
 
-  const categories = ["Men's Clothing", "Women's Clothing", "Electronics", "Jewelery"]
+  const categories = [
+    {
+      name: "Men's Clothing",
+      fetchUrl: "https://fakestoreapi.com/products/category/men's%20clothing"
+    },
+    {
+      name: "Women's Clothing",
+      fetchUrl: "https://fakestoreapi.com/products/category/women's%20clothing"
+    },
+    {
+      name: "Electronics",
+      fetchUrl: "https://fakestoreapi.com/products/category/electronics"
+    },
+    {
+      name: "Jewelery",
+      fetchUrl: "https://fakestoreapi.com/products/category/jewelery"
+    }
+  ]
 
-  const handleCategory = (index) => {
+  const handleCategory = (index, fetchUrl) => {
     setActiveCategory(index)
-  }
-
-  if (isLoading) {
-    return (
-      <div className={styles.loading}>
-        <ReactLoading type='spin' color='#000000' />
-      </div>
-    )
+    setUrl(fetchUrl)
   }
 
   return (
     <div className={styles.shop}>
-      <h1>{activeCategory !== null ? categories[activeCategory] : 'All Products'}</h1>
+      <h1>{activeCategory !== null ? categories[activeCategory].name : 'All Products'}</h1>
       <div className={styles['category-container']}>
         <h2>Categories</h2>
         <ul>
           {categories.map((category, index) => (
             <li
             key={index}
-            onClick={() => handleCategory(index)}
+            onClick={() => handleCategory(index, category.fetchUrl)}
             className={activeCategory === index ? styles.active : ''}
             >
-              {category}
+              {category.name}
             </li>
           ))}
         </ul>
       </div>
-      <div className={styles['product-container']}>
-        <span>{data.length} items</span>
-        <div className={styles['product-list']}>
-          {data.map(product => <Product data={product} key={product.id}/>)}
+      {
+        isLoading ? 
+        <div className={styles.loading}>
+          <ReactLoading type='spin' color='#000000' />
         </div>
-      </div>
+        : <ProductList data={data} />
+      }
+      
     </div>
   )
 }
