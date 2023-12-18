67301bcb56188e75277e4f9590640d3834518bb1
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-18 12:13:50 +0700

    Add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { useState, useEffect } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './components/App/App'
+import Hero from './components/Hero/Hero'
+import Shop from './components/Shop/Shop'
 import './index.css'
 import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
 import '@fontsource-variable/inter'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Hero /> },
+      { path: '/shop', element: <Shop />}
+    ]
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
