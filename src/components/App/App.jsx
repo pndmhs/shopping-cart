c3bd61940ddbc0804443dbcb33d7bdcbeca9f1ed
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-18 12:14:37 +0700

    Remove useFetch and add Outlet component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,32 +1,13 @@
-import { useState, useEffect } from 'react'
+import { Outlet } from 'react-router-dom'
 import styles from './App.module.css'
 import Header from '../Header/Header'
-import Hero from '../Hero/Hero'
-import Shop from '../Shop/Shop'
-
-const useFetch = (url) => {
-  const [data, setData] = useState(null)
-  const [err, setErr] = useState(null)
-  const [isLoading, setLoading] = useState(true)
-
-  useEffect(() => {
-    fetch(url)
-    .then(response => response.json())
-    .then(data => setData(data))
-    .catch(err => setErr(err))
-    .finally(() => setLoading(false))
-  }, [])
-
-  return { data, err, isLoading }
-}
 
 const App = () => {
 
   return (
     <div className={styles.container}>
       <Header />
-      {/* <Hero /> */}
-      <Shop useFetch={useFetch}/>
+      <Outlet />
     </div>
   )
 }
