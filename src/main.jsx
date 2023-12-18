ca77a6886c7fdb498eadd7d1d96286ca63025909
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-18 12:49:51 +0700

    Remove useState and useEffect import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { useState, useEffect } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './components/App/App'
 import Hero from './components/Hero/Hero'
