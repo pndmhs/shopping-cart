439d32bebb71a62ed2af48942a7ead2c24342929
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-20 05:51:53 +0700

    Fix router import

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
 import '@fontsource-variable/inter'
-import Router from './components/Router/Router'
+import Router from './Router/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
