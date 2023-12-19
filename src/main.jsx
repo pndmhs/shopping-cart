ad13c1a06bc94469a97d841efe5f51c6dc8e78cd
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-20 05:30:24 +0700

    Move router to seperate file

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './components/App/App'
-import Hero from './components/Hero/Hero'
-import Shop from './components/Shop/Shop'
 import './index.css'
 import '@fontsource/poppins/600.css'
 import '@fontsource/poppins/700.css'
 import '@fontsource-variable/inter'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      { path: '/', element: <Hero /> },
-      { path: '/shop', element: <Shop />}
-    ]
-  },
-])
+import Router from './components/Router/Router'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>,
 )
