aa9e89a7ed9caaeab5a02f3e47df8c1f9e85eded
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-23 21:32:00 +0700

    Add router to show product detail page

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,15 +2,24 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from '../components/App/App'
 import Hero from '../components/Hero/Hero'
 import Shop from '../components/Shop/Shop'
+import { useState } from 'react'
+import ProductDetail from '../components/ProductDetail/ProductDetail'
 
 const Router = () => {
+
+  const [productID, setProductID] = useState(null)
+
+  const handleProduct = (id) => {
+    setProductID(id)
+  }
   const router = createBrowserRouter([
     {
       path: '/',
       element: <App />,
       children: [
         { path: '/', element: <Hero /> },
-        { path: '/shop', element: <Shop />}
+        { path: '/shop', element: <Shop setProductID={handleProduct}/>},
+        { path: '/shop/product', element: <ProductDetail productID={productID} />}
       ]
     },
   ])
