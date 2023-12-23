8243892804e3bbfcb461b1229e64f0bf0a662c8e
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-23 21:34:22 +0700

    Display product from fetched data

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,13 +1,28 @@
+import useFetch from '../../Hooks/useFetch'
+import Error from '../Error/Error'
+import Loading from '../Loading/Loading'
 import styles from './ProductDetail.module.css'
 
-const ProductDetail = () => {
+const ProductDetail = ({ productID }) => {
+  const { data, err, isLoading } = useFetch(`https://fakestoreapi.com/products/${productID}`)
+
+  if (err) {
+    return <Error message={err} />
+  }
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <div className={styles['product-detail']}>
-      <div className={styles['product-img']}></div>
+      <div className={styles['product-img']}>
+        <img src={data.image} alt={data.title} />
+      </div>
       <div className={styles['product-info']}>
-        <h1>Mens Casual Premium Slim Fit T-Shirts</h1>
-        <p className={styles['product-price']}>$199.99</p>
-        <p className={styles['product-desc']}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tristique nulla aliquet enim tortor at auctor urna.</p>
+        <h1>{data.title}</h1>
+        <p className={styles['product-price']}>${data.price}</p>
+        <p className={styles['product-desc']}>{data.description}</p>
         <label htmlFor="quantity" className={styles['quantity']}>
           Quantity:
           <input type="number" name="quantity" />
