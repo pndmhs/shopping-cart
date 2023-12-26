eada892c562fd3b8e149b475e6224b554f6ebf76
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-26 10:02:20 +0700

    Add search result component

---
+++ src/components/SearchResult/SearchResult.jsx
@@ -0,0 +1,33 @@
+import useFetch from '../../Hooks/useFetch'
+import Product from '../ProductList/Product'
+import Loading from '../Loading/Loading'
+import Error from '../Error/Error'
+import styles from './SearchResult.module.css'
+
+const SearchResult = ({ searchQuery, setProductID }) => {
+  const { data, err, isLoading } = useFetch('https://fakestoreapi.com/products')
+
+  const filterData = (data) => {
+    return data.filter(product => product.title.toLowerCase().includes(searchQuery.toLowerCase()))
+  }
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  if (err) {
+    return <Error message={err}/>
+  }
+
+  return (
+    <div className={styles['search-result-container']}>
+      <h1>Search result for "{searchQuery}"</h1>
+      {filterData(data).length === 0 && <p>No items found, please try again</p>}
+      <div className={styles['search-result']}>
+        {filterData(data).length > 0 && filterData(data).map(product => <Product key={product.id} data={product} setProductID={setProductID} />)}
+      </div>
+    </div>
+  )
+}
+
+export default SearchResult
