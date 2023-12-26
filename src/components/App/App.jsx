d2a8c9df2d298cbdbc486e15583a88805d6244a7
Pandu Mahesa <pandu.mahesa9@gmail.com>
2023-12-26 10:03:01 +0700

    Add handleSearch prop

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,11 +2,11 @@ import { Outlet } from 'react-router-dom'
 import styles from './App.module.css'
 import Header from '../Header/Header'
 
-const App = ({ cartItems, changeQuantity, deleteProduct }) => {
+const App = ({ cartItems, changeQuantity, deleteProduct, handleSearch }) => {
 
   return (
     <div className={styles.container}>
-      <Header cartItems={cartItems} changeQuantity={changeQuantity} deleteProduct={deleteProduct} />
+      <Header cartItems={cartItems} changeQuantity={changeQuantity} deleteProduct={deleteProduct} handleSearch={handleSearch} />
       <Outlet />
     </div>
   )
