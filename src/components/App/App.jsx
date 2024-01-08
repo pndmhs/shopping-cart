8dd4fbb4383032c62179b181c12cab8dfe0a38a6
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-08 10:17:19 +0700

    Add context to manage state

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,68 @@
 import { Outlet } from 'react-router-dom'
 import styles from './App.module.css'
 import Header from '../Header/Header'
+import { createContext } from 'react'
+import { useState } from 'react'
 
-const App = ({ cartItems, changeQuantity, deleteProduct, handleSearch }) => {
+export const ShopContext = createContext({
+  cartItems: [],
+  searchQuery: null,
+  productID: null,
+  handleProduct: () => {},
+  addToCart: () => {},
+  changeQuantity: () => {},
+  deleteProduct: () => {},
+  handleSearch: () => {}
+})
+
+const App = () => {
+  const [productID, setProductID] = useState(null)
+
+  const [cartItems, setCartItems] = useState([])
+
+  const [searchQuery, setSearchQuery] = useState(null)
+
+  const handleProduct = (id) => {
+    setProductID(id)
+  }
+
+  const addToCart = (product) => {
+    const existedProduct = cartItems.find(item => item.id === product.id)
+    if (existedProduct) {
+      changeQuantity(product.id, product.quantity + existedProduct.quantity)
+      return
+    }
+
+    setCartItems([...cartItems, product])
+  }
+
+  const changeQuantity = (productID, quantity) => {
+    const updatedCart = cartItems.map(item => {
+      if (item.id === productID) {
+        return { ...item, quantity}
+      }
+
+      return item
+    })
+
+    setCartItems(updatedCart)
+  }
+
+  const deleteProduct = (productID) => {
+    const updatedCart = cartItems.filter((item) => item.id !== productID)
+    setCartItems(updatedCart)
+  }
+
+  const handleSearch = (searchQuery) => {
+    setSearchQuery(searchQuery)
+  }
 
   return (
     <div className={styles.container}>
-      <Header cartItems={cartItems} changeQuantity={changeQuantity} deleteProduct={deleteProduct} handleSearch={handleSearch} />
-      <Outlet />
+      <ShopContext.Provider value={{ productID, cartItems, searchQuery, handleProduct, addToCart, changeQuantity, handleSearch, deleteProduct }}>
+        <Header />
+        <Outlet />
+      </ShopContext.Provider>
     </div>
   )
 }
