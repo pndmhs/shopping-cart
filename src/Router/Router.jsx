1f9aabd89684ba3fc2ac7d4f10e454bf67dbb5d8
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-08 10:22:45 +0700

    Remove state, functions, and props

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -8,56 +8,15 @@ import SearchResult from '../components/SearchResult/SearchResult'
 
 const Router = () => {
 
-  const [productID, setProductID] = useState(null)
-
-  const [cartItems, setCartItems] = useState([])
-
-  const [searchQuery, setSearchQuery] = useState(null)
-
-  const handleProduct = (id) => {
-    setProductID(id)
-  }
-
-  const addToCart = (product) => {
-    const existedProduct = cartItems.find(item => item.id === product.id)
-    if (existedProduct) {
-      changeQuantity(product.id, product.quantity + existedProduct.quantity)
-      return
-    }
-
-    setCartItems([...cartItems, product])
-  }
-
-  const changeQuantity = (productID, quantity) => {
-    const updatedCart = cartItems.map(item => {
-      if (item.id === productID) {
-        return { ...item, quantity}
-      }
-
-      return item
-    })
-
-    setCartItems(updatedCart)
-  }
-
-  const deleteProduct = (productID) => {
-    const updatedCart = cartItems.filter((item) => item.id !== productID)
-    setCartItems(updatedCart)
-  }
-
-  const handleSearch = (searchQuery) => {
-    setSearchQuery(searchQuery)
-  }
-
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <App cartItems={cartItems} changeQuantity={changeQuantity} deleteProduct={deleteProduct} handleSearch={handleSearch} />,
+      element: <App />,
       children: [
         { path: '/', element: <Hero /> },
-        { path: '/shop', element: <Shop setProductID={handleProduct}/>},
-        { path: '/shop/product', element: <ProductDetail productID={productID} addToCart={addToCart} />},
-        { path: '/search', element: <SearchResult searchQuery={searchQuery} setProductID={handleProduct} /> }
+        { path: '/shop', element: <Shop />},
+        { path: '/shop/product', element: <ProductDetail />},
+        { path: '/search', element: <SearchResult /> }
       ]
     },
   ])
