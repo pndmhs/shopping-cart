473906a71a00145ec04093e78a6e5528b9270753
Pandu Mahesa <pandu.mahesa9@gmail.com>
2024-01-11 22:27:06 +0700

    Add route parameter to product detail

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from '../components/App/App'
 import Hero from '../components/Hero/Hero'
 import Shop from '../components/Shop/Shop'
-import { useState } from 'react'
 import ProductDetail from '../components/ProductDetail/ProductDetail'
 import SearchResult from '../components/SearchResult/SearchResult'
 
@@ -15,7 +14,7 @@ const Router = () => {
       children: [
         { path: '/', element: <Hero /> },
         { path: '/shop', element: <Shop />},
-        { path: '/shop/product', element: <ProductDetail />},
+        { path: '/shop/product/:productID', element: <ProductDetail />},
         { path: '/search', element: <SearchResult /> }
       ]
     },
